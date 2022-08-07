e68c71c1cd81eac0e734a68f11e94915dd0365a7
Austin Hwang <austin.hwang.dev@gmail.com>
2022-08-07 22:39:36 +0900

    Interpunct-generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import checkImg from "./img/checkImg.png";
+import githubImg from "./img/githubImg.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//  버튼을 눌리기 전 = 0, 버튼을 눌린 후 = 1
+	let [copiedOrNot, setCopiedOrNot] = useState("0");
+	// 5초 후 버튼 초기화
+	let btnReturn = function () {
+		setTimeout(function () {
+			setCopiedOrNot("0");
+		}, 5000);
+	};
+	return (
+		<div className="App">
+			<CopyToClipboard text="·">
+				<div className="mb-2">
+					<Button
+						variant="secondary"
+						size="lg"
+						onClick={() => {
+							setCopiedOrNot("1");
+							btnReturn();
+						}}
+					>
+						{copiedOrNot === "0" ? (
+							<div>가운뎃점 복사</div>
+						) : (
+							<div className="copiedBtn">
+								<img className="checkImg" src={checkImg} />
+								<div>복사완료!</div>
+							</div>
+						)}
+					</Button>
+				</div>
+			</CopyToClipboard>
+			<a
+				href="https://github.com/austin-verse/Interpunct-generator"
+				target="_blank"
+			>
+				<Button type="button" className="githubBtn">
+					<img src={githubImg} />
+				</Button>
+			</a>
+			<link
+				rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+			></link>
+		</div>
+	);
 }
 
 export default App;
